fbd9de4db2021388a71799abad1956ba22a4eb39
Microwavekonijn <microwavekonijn@pm.me>
2022-06-19 16:17:09 +0200

    chore: remove redundant redis module

--- src/redis/redis.config.ts
+++
@@ -1,8 +0,0 @@
-import { IsUrl } from 'class-validator';
-import { ProcessEnv } from '@census-reworked/nestjs-utils';
-
-export class RedisConfig {
-  @ProcessEnv('REDIS_URL')
-  @IsUrl({ protocols: ['redis'] })
-  url = 'redis://localhost:27017';
-}
