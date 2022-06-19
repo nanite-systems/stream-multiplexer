e282f0ba6883942b8b71e86a76243b54b7ae562e
Microwavekonijn <microwavekonijn@pm.me>
2022-06-19 23:33:42 +0200

    fix: world tracker import

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,13 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@census-reworked/nestjs-utils';
 import { AppConfig } from './app.config';
 import { IngressModule } from './ingress/ingress.module';
+import { WorldTrackerModule } from './world-tracker/world-tracker.module';
 
 @Module({
-  imports: [ConfigModule.forFeature([AppConfig]), IngressModule],
+  imports: [
+    ConfigModule.forFeature([AppConfig]),
+    IngressModule,
+    WorldTrackerModule,
+  ],
 })
 export class AppModule {}
--- src/world-tracker/world-tracker.module.ts
+++ src/world-tracker/world-tracker.module.ts
@@ -3,10 +3,11 @@ import { CensusModule } from '../census/census.module';
 import { WorldStateController } from './controllers/world-state.controller';
 import { WorldStateService } from './services/world-state.service';
 import { PublisherModule } from '../publisher/publisher.module';
+import { WorldTracker } from './trackers/world.tracker';
 
 @Module({
   imports: [CensusModule, PublisherModule],
-  providers: [WorldStateService],
+  providers: [WorldStateService, WorldTracker],
   controllers: [WorldStateController],
 })
 export class WorldTrackerModule {}
